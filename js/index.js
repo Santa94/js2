970d4f9d397eb958a91afc1f8a366ceaff41045c
Yana Byldina <yanabyldina@gmail.com>
2018-12-18 15:41:13 +0000

    project Created

--- js/index.js
+++ js/index.js
@@ -1,25 +1,28 @@
+const enterNumber = prompt('Enter Number');
 
-/*function fib(n) {
-let first = 1;
-let second= 1;
-for  ( i=3 ; i <= n ; i++ ) {
-    let next = first + second ;
-    first=second;
-    second=next;
-    
-}
-return second ;
-}
-*/
-const enterNumber= prompt('Enter Number') ;
 function fib(n) {
-  let first = 1,
-    second = 1;
-  for (let i = 3; i <= n; i++) {
-    let next = first + second;
-    first = second;
-    second = next;
-  }
-  return second;
+    let first = 1,
+        second = 1;
+    for (let i = 3; i <= n; i++) {
+        let next = first + second;
+        first = second;
+        second = next;
+    }
+    return second;
 }
-alert( fib(enterNumber) );
+alert(fib(enterNumber)); 
+
+
+
+/*let n=15,a,b;
+let fib = (1,1)
+fib.push(1,1) 
+while (fib.length < n) {
+    a=fib.length[n - 1]
+    b=fib.length[n - 2]
+    fib.push(a+b)
+}
+
+for (let i = 0; fib.length > i; i++) {
+    alert(fib[i]);
+}
